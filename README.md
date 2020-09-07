e19aeab004763cd74596e7290b87ab3b0b97e25d
MenkeTechnologies <linux.dev25@gmail.com>
2020-09-07 04:38:10 -0400

    why this exists

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # zsh-zinit-final
 # created by MenkeTechnologies
+
+
+This is a placeholder plugin for zinit atinit and atload ices.
---
+++ zsh-zinit-final.plugin.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+#{{{                    MARK:Header
+#**************************************************************
+##### Author: MenkeTechnologies
+##### GitHub: https://github.com/MenkeTechnologies
+##### Date: Mon Sep  7 04:37:11 EDT 2020
+##### Purpose: zsh script to
+##### Notes:
+#}}}***********************************************************
+
+
